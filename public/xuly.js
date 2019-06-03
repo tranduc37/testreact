ed5dc0c3af1eaeed250c10c2a80a1fcd835fc680
Thong Dinh <thong@Soas-Air.net.fpt>
2019-06-04 03:22:32 +0700

    state firt

--- public/xuly.js
+++ public/xuly.js
@@ -1,17 +1,23 @@
 function getName(name){
     alert(name);
-}
+};
 
 var TranDuc = React.createClass({
-
+    // addDay (){
+    //     this.setState({tongSoNgay: this.state.tongSoNgay +1});
+    // },
     layThongTin: function() {
         alert(this.props.children);
+    },
+    getInitalState(){
+        return {tongSoNgay: 0};
     },
         render : function(){
             return(
                 <div>
                     <h1 className="color"> {this.props.ten} - {this.props.time}</h1>
                     <p>{this.props.children}</p>
+                    <div>So Ngay: {this.state.tongSoNgay}</div>
                     <button onClick={()=>{var str =this.props.ten + ' ' + this.props.time; getName(str)}}>LAY THONG TIN</button>
                     <KhoaHoc/>
                 </div>
@@ -21,7 +27,7 @@ var TranDuc = React.createClass({
 var KhoaHoc = React.createClass({
     render: function(){
         return(
-            <h3>2 week</h3>
+            <h3>work hard</h3>
         );
     }
 });
